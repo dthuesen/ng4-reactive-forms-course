febde73dc679d6545eb0974a787533f91d3496fd
dthuesen <dthuesen@gmx.de>
2017-07-13 17:56:23 +0200

    Add custom validation with parameters

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,7 +1,18 @@
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
+
+// the factory function which takes two parameters (min and max acceptable value)
+function ratingRange(min: number, max: number): ValidatorFn {
+  // custom validator
+  return (control: AbstractControl): {[key: string]: boolean} | null => {
+    if (control.value !== undefined && (isNaN(control.value) || control.value < min || control.value > max)) {
+      return { 'range': true };
+    };
+    return null
+  }
+}
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -35,6 +46,9 @@ export class CustomersComponent implements OnInit {
         firstName: ['', [Validators.required, Validators.minLength(3)]],
         lastName: ['', [Validators.required, Validators.maxLength(50)]],
         email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]+')]],
+        phone: '',
+        notification: 'email',
+        rating: ['', ratingRange(1, 5)],
         sendCatalog: true
     });
   }
@@ -50,6 +64,16 @@ export class CustomersComponent implements OnInit {
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 
 }
