60c89b3358f34fae85b3dc59ca9db39b47e983d2
dthuesen <dthuesen@gmx.de>
2017-07-14 12:44:33 +0200

    Add cross-field validation - not yet ready coded

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -45,7 +45,10 @@ export class CustomersComponent implements OnInit {
     this.customerForm = this.fb.group({
         firstName: ['', [Validators.required, Validators.minLength(3)]],
         lastName: ['', [Validators.required, Validators.maxLength(50)]],
-        email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]+')]],
+        emailGroup: this.fb.group({ // <-- the nested FormGroup
+          email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]+')]],
+          confirmEmail: ['', [Validators.required]], // <-- here - no pattern is needed for comparison
+        }),
         phone: '',
         notification: 'email',
         rating: ['', ratingRange(1, 5)],
